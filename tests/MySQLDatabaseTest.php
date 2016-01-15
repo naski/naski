ec6ae5594d3fe092912b152bf5416da7532407d9
Doelia <kyxalia@gmail.com>
2016-01-15 15:42:49 +0100

    Add createTable test

--- tests/MySQLDatabaseTest.php
+++ tests/MySQLDatabaseTest.php
@@ -6,14 +6,29 @@ use DoePdo\MySQLDatabase;
 
 class MySQLDatabaseTest extends PHPUnit_Framework_TestCase
 {
+    private $_bd;
+
     public function testConnect()
     {
-        $db = new MySQLDatabase();
-        $db->connect(array(
+        $this->_db = new MySQLDatabase();
+        $this->_db->connect(array(
             'host' => '127.0.0.1',
             'dbname' => 'test',
             'username' => 'root',
             'password' => '',
         ));
+    }
+
+    public function testCreateTable() {
+        $this->_db->query("
+        DROP TABLE IF EXISTS `tests`;
+        CREATE TABLE `tests` (
+          `ID` int(11) NOT NULL AUTO_INCREMENT,
+          `row1` varchar(255) DEFAULT NULL,
+          `row2` varchar(255) DEFAULT NULL,
+          `row3` int(11) DEFAULT NULL,
+          PRIMARY KEY (`ID`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+        ");
     }
 }
