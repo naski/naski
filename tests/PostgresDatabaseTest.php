aa2ee0a3bfaff648782b4c717aa2fee40cef58ce
Doelia <kyxalia@gmail.com>
2016-01-23 19:33:40 +0100

    Correction du test postgres

--- tests/PostgresDatabaseTest.php
+++ tests/PostgresDatabaseTest.php
@@ -20,16 +20,14 @@ class PostgresDatabaseTest extends AbstractTester
 
     public function testCreateTable()
     {
-        $this->_db->query('
-        DROP TABLE IF EXISTS "public"."tests";
-        CREATE TABLE "public"."tests" (
-        	"ID" int4 NOT NULL,
-        	"row1" varchar(255) COLLATE "default",
-        	"row2" varchar(255) COLLATE "default",
-        	"row3" int4
-        );
-        ALTER TABLE "public"."tests" ADD PRIMARY KEY ("ID") NOT DEFERRABLE INITIALLY IMMEDIATE;
-        ');
+        $this->_db->query('DROP TABLE IF EXISTS "public"."tests";');
+        $this->_db->query('CREATE TABLE "public"."tests" (
+            	"ID" int4 NOT NULL,
+            	"row1" varchar(255) COLLATE "default",
+            	"row2" varchar(255) COLLATE "default",
+            	"row3" int4
+            );');
+        $this->_db->query('ALTER TABLE "public"."tests" ADD PRIMARY KEY ("ID") NOT DEFERRABLE INITIALLY IMMEDIATE;');
     }
 
 
