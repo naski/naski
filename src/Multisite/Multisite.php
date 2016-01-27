4e38836753433c5654e90a07cfbe9d7c68002c0e
Doelia <kyxalia@gmail.com>
2016-01-27 13:22:06 +0100

    Revert "Retrait du multi-site pour isolation dans un nouveau composant"
    This reverts commit 40567e8e3946ae288ac508d995cdb088c1be354c.

---
+++ src/Multisite/Multisite.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Naski\Routing\Multisite;
+
+use Naski\Config\Config;
+use Psr\Http\Message\UriInterface;
+
+class Multisite
+{
+    private $root = null; // Chemin absolu
+    private $_websites = array(); // array<array>
+
+    public static function buildFromConfig(Config $config, string $rootSystem): self
+    {
+        $obj = new self($rootSystem.$config['rootPath']);
+        foreach ($config['websites'] as $w) {
+            $obj->addSite(new Site($w));
+        }
+
+        return $obj;
+    }
+
+    public function __construct(string $root)
+    {
+        $this->_root = $root;
+    }
+
+    public function addSite(Site $site)
+    {
+        $this->_websites[] = $site;
+    }
+
+    public function process(UriInterface $uri)
+    {
+        foreach ($this->_websites as $w) {
+            if ($w->match($uri)) {
+                $w->exec($this->_root, $uri);
+
+                return $w;
+            }
+        }
+
+        return;
+    }
+}
---
+++ tests/MultisiteTest.php
@@ -0,0 +1,79 @@
+<?php
+
+require_once 'bootstrap.php';
+
+use Naski\Config\Config;
+use Naski\Routing\Multisite\Multisite;
+use Naski\Routing\Multisite\Site;
+use League\Uri\Schemes\Http as HttpUri;
+
+class MultisiteTest extends PHPUnit_Framework_TestCase
+{
+    public function testDisplay()
+    {
+        $multisite = new Multisite(__DIR__.'/');
+        $multisite->addSite($site1 = new Site(array(
+            'name' => 'Site 1',
+            'initFile' => 'initSite.php',
+        )));
+        $out = $multisite->process(HttpUri::createFromString('http://doelia.fr/'));
+
+        $this->expectOutputString('Site 1');
+    }
+
+    public function testMatch()
+    {
+        $multisite = new Multisite(__DIR__.'/');
+        $multisite->addSite($site2 = new Site(array(
+            'name' => 'Site 2',
+            'conditions' => array(
+                'path' => '^/site2(.*)',
+            ),
+            'initFile' => 'initSite.php',
+        )));
+        $multisite->addSite($site1 = new Site(array(
+            'name' => 'Site 1',
+            'initFile' => 'initSite.php',
+        )));
+
+        $out = $multisite->process(HttpUri::createFromString('http://doelia.fr/'));
+        $this->assertEquals($out, $site1);
+        $this->assertNotEquals($out, $site2);
+
+        $out = $multisite->process(HttpUri::createFromString('http://doelia.fr/site2/gogo'));
+        $this->assertEquals($out, $site2);
+        $this->assertNotEquals($out, $site1);
+    }
+
+    public function testLoadFile()
+    {
+        $websites = new Config();
+        $websites->loadJSONFile(__DIR__.'/multisite.json');
+        $multisite = MultiSite::buildFromConfig($websites, __DIR__);
+        $out = $multisite->process(HttpUri::createFromString('http://doelia.fr/product'));
+        $this->assertEquals($out->name, 'Site 1');
+        $this->assertNotEquals($out->name, 'Site 2');
+    }
+
+    public function testHttps()
+    {
+        $websites = new Config();
+        $websites->loadJSONFile(__DIR__.'/multisite.json');
+        $multisite = MultiSite::buildFromConfig($websites, __DIR__);
+        $out = $multisite->process(HttpUri::createFromString('https://doelia.fr/'));
+        $this->assertEquals($out->name, 'Site HTTPS');
+        $out = $multisite->process(HttpUri::createFromString('http://doelia.fr/'));
+        $this->assertNotEquals($out->name, 'Site HTTPS');
+    }
+
+    public function testHost()
+    {
+        $websites = new Config();
+        $websites->loadJSONFile(__DIR__.'/multisite.json');
+        $multisite = MultiSite::buildFromConfig($websites, __DIR__);
+        $out = $multisite->process(HttpUri::createFromString('http://vps.doelia.fr/'));
+        $this->assertEquals($out->name, 'Site privé');
+        $out = $multisite->process(HttpUri::createFromString('http://doelia.fr/'));
+        $this->assertNotEquals($out->name, 'Site privé');
+    }
+}
