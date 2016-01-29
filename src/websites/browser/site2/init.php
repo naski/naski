9d2214295d6fc4660ba8b2be347b083da6290997
Doelia <kyxalia@gmail.com>
2016-01-29 18:43:23 +0100

    Tests assetics (en cours)

--- src/websites/browser/site2/init.php
+++ src/websites/browser/site2/init.php
@@ -6,6 +6,24 @@ global $IM;
 
 $IM->recordInstance('site', $SITE);
 
+use Assetic\Asset\AssetCollection;
+use Assetic\Asset\FileAsset;
+use Assetic\Asset\GlobAsset;
+use Assetic\AssetWriter;
+use Assetic\FilterManager;
+use Assetic\Filter\Sass\SassFilter;
+use Assetic\Filter\Yui;
+use Assetic\AssetManager;
+
+
+$js = new AssetCollection(array(
+    new FileAsset(ROOT_SYSTEM_WEBSITE.'hey.css')
+));
+
+
+// the code is merged when the asset is dumped
+echo $js->dump();
+
 $IM->twig->addTwigPath(ROOT_SYSTEM_WEBSITE);
 $IM->twig->useBundle('devBar');
 $IM->twig->render('index.twig');
