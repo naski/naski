0a70b987ab1fdb4b15fa33182a72a2aea3a83ea4
Doelia <kyxalia@gmail.com>
2016-02-18 20:09:12 +0100

    Configuration gitlab runner

--- docker_install.sh
+++ docker_install.sh
@@ -5,19 +5,10 @@
 
 set -xe
 
-# Install exention
-apt-get install -y \
-        libfreetype6-dev \
-        libjpeg62-turbo-dev \
-        libmcrypt-dev \
-        libpng12-dev \
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
 apt-get install git -yqq
-
-
-
 
 # Install phpunit, the tool that we will use for testing
 apt-get install -y phpunit
