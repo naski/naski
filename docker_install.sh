cdc2806969100102e81e22f3737d68a2df65b20a
Doelia <kyxalia@gmail.com>
2016-02-18 20:19:06 +0100

    Configuration gitlab runner

--- docker_install.sh
+++ docker_install.sh
@@ -9,6 +9,7 @@ set -xe
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
 apt-get install git -yqq
+apt-get install -y zlib1g-dev libicu-dev g++
 
 # Install phpunit, the tool that we will use for testing
 apt-get install -y phpunit
