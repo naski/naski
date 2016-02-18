374fe1bde18674496cd78511c027c08186b89f75
Doelia <kyxalia@gmail.com>
2016-02-18 14:19:48 +0100

    Configuration gitlab runner

--- docker_install.sh
+++ docker_install.sh
@@ -8,7 +8,8 @@ set -xe
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
 apt-get install git -yqq
-apt-get install php-mbstring yqq
+apt-get install -y phpunit
+#apt-get install php-mbstring yqq
 
 # Install phpunit, the tool that we will use for testing
 curl -o /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
