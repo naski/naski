ddc6e7708649cf92af9c4aaf49d819c1076dd103
Doelia <kyxalia@gmail.com>
2016-02-18 14:32:18 +0100

    Configuration gitlab runner

--- docker_install.sh
+++ docker_install.sh
@@ -9,8 +9,22 @@ set -xe
 apt-get update -yqq
 apt-get install git -yqq
 apt-get install -y phpunit
-apt-get install php5-mbstring yqq
 
 # Install phpunit, the tool that we will use for testing
 curl -o /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
 chmod +x /usr/local/bin/phpunit
+
+curl -fsSL 'https://xcache.lighttpd.net/pub/Releases/3.2.0/xcache-3.2.0.tar.gz' -o xcache.tar.gz
+
+mkdir -p xcache
+tar -xf xcache.tar.gz -C xcache --strip-components=1
+rm xcache.tar.gz
+
+cd xcache
+phpize
+./configure --enable-xcache
+make -j$(nproc)
+make install
+
+rm -r xcache \
+docker-php-ext-enable xcache
