6bf6279a22b97eea77d28333ba8090f60f8a48c1
Doelia <kyxalia@gmail.com>
2016-02-20 21:37:44 +0100

    Correction tests unitaires

--- tests/autoload.php
+++ tests/autoload.php
@@ -7,11 +7,9 @@ define('ROOT_SYSTEM', __DIR__.'/');
 define('NASKI_CORE_PATH', __DIR__.'/../core/');
 define('NASKI_APP_PATH', __DIR__.'/app/');
 
-trigger_error('boom', E_USER_ERROR);
-
 // Classes des composants externes
 if (!require_once __DIR__.'/../vendor/autoload.php') {
-    trigger_error('Exécuter `composer install` pour télécharger le vendor/');
+    trigger_error('Exécuter `composer install` pour télécharger le vendor/', E_USER_ERROR);
 }
 
 // Initialisation du framework
