e3ba01e7d5b8131c074474143d85c8f5fea81123
Doelia <kyxalia@gmail.com>
2016-02-20 21:36:22 +0100

    Correction tests unitaires

--- core/boot/init.php
+++ core/boot/init.php
@@ -3,7 +3,7 @@
 $globals_needles = ['ROOT_SYSTEM', 'NASKI_CORE_PATH', 'NASKI_APP_PATH'];
 foreach ($globals_needles as $v) {
     if (!defined($v)) {
-        die("La globale $v n'est pas définie dans /boot.php");
+        trigger_error("La globale $v n'est pas définie dans /boot.php");
     }
 }
 
--- tests/autoload.php
+++ tests/autoload.php
@@ -1,11 +1,13 @@
 <?php
 
 ini_set("display_errors", 1);
-error_report ing(E_ALL);
+error_reporting(E_ALL);
 
 define('ROOT_SYSTEM', __DIR__.'/');
 define('NASKI_CORE_PATH', __DIR__.'/../core/');
 define('NASKI_APP_PATH', __DIR__.'/app/');
+
+trigger_error('boom');
 
 // Classes des composants externes
 if (!require_once __DIR__.'/../vendor/autoload.php') {
