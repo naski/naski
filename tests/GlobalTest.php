22233946dc0a09f01550eb7bb249e6be25fbc2c8
Doelia <kyxalia@gmail.com>
2016-02-20 22:11:26 +0100

    Ajout test unitaires

--- tests/GlobalTest.php
+++ tests/GlobalTest.php
@@ -14,5 +14,7 @@ class GlobalTesteur extends PHPUnit_Framework_TestCase
         $out = array();
         exec('php tests/console.php cleanLogs go 2>&1', $out);
         $this->assertContains('app/logs', $out[1]);
+
+        $this->assertContains('rm -rf', exec('php tests/console.php cleanCache 2>&1'));
     }
 }
