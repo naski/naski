f3415237d7e7bd31ce23d57956f10f03d67d6b4e
Doelia <kyxalia@gmail.com>
2016-05-22 15:49:09 +0200

    Correction config CI

--- tests/github-ci/Pdo/config.php
+++ tests/github-ci/Pdo/config.php
@@ -4,12 +4,12 @@ $GLOBALS['DB_MYSQL'] = array(
     'host' => '127.0.0.1',
     'dbname' => 'tests',
     'username' => 'postgres',
-    'password' => 'postgres'
+    'password' => 'github-ci'
 );
 
 $GLOBALS['DB_POSTGRES'] = array(
     'host' => '127.0.0.1',
     'dbname' => 'tests',
     'username' => 'postgres',
-    'password' => 'postgres'
+    'password' => 'github-ci'
 );
