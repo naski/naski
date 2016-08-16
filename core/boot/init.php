92f16d772a03ec571f142054e4998daadd62327e
Doelia <kyxalia@gmail.com>
2016-08-16 09:36:26 +0200

    Retrait des scripts non utilisés

--- core/boot/init.php
+++ core/boot/init.php
@@ -32,11 +32,3 @@ if ($IM->config['show_php_errors'] ?? false) {
 } else {
     ini_set("display_errors", 0);
 }
-
-// Ajout de quelques commandes à la console
-\Naski\Console::getInstance()->recordCommand("cleanCache", array(new \Naski\Controllers\Backend(), 'cleanCache'));
-\Naski\Console::getInstance()->recordCommand("cleanLogs", array(new \Naski\Controllers\Backend(), 'cleanLogs'));
-\Naski\Console::getInstance()->recordCommand("clearAssets", function() {
-    echo exec("rm -rf ".ROOT_SYSTEM.'web/generated_assets');
-});
-\Naski\Console::getInstance()->recordFileExec("setPerms", NASKI_CORE_PATH.'scripts/set_perms.sh');
