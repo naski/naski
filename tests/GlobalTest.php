2c6180dac38cb69ab4c0d3d4d2e0da53b61038cf
Doelia <kyxalia@gmail.com>
2016-08-16 09:38:31 +0200

    Correction des tests d'integration

--- tests/GlobalTest.php
+++ tests/GlobalTest.php
@@ -9,18 +9,6 @@ class GlobalTesteur extends PHPUnit_Framework_TestCase
 
     }
 
-    public function testExecs()
-    {
-        $out = array();
-        exec('php tests/console.php cleanLogs go 2>&1', $out);
-        $this->assertContains('clean_logs.sh go', $out[0]);
-
-        $out = array();
-        exec('php tests/console.php cleanCache 2>&1', $out);
-        $this->assertContains('clean_cache.sh go', $out[0]);
-
-    }
-
     public function testArrayKeep()
     {
         $in = array(
