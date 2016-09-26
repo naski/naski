f9e0c884781ca8c853df37bbc89f94528faed7fc
Doelia <kyxalia@gmail.com>
2016-09-26 11:57:09 +0200

    Renomage de la constante BIG INT

--- core/boot/init.php
+++ core/boot/init.php
@@ -12,7 +12,7 @@ foreach ($globals_needles as $v) {
 use Naski\Config\Config;
 use Naski\InstancesManager;
 
-define('BIG_INT', 987654321);
+define('INF_INT', 987654321);
 
 
 // Variable globale contenant toutes les instances à déclarer comme globales
